fb4f3fb8af11085ad76a78dbfd7bb176aa6cb62e
AmirSiwal <amirsilwal5@gmai.com>
2022-12-21 12:57:59 +0545

    final commit

---
+++ product_module/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Brand, Category, Product, CartItem
+# Register your models here.
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(CartItem)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["image_tag", "name", "price", "brand", "category",]
+    search_fields =["name","price", "brand__name", "category__name",]
+    # readonly_fields = ["quantity"]
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
